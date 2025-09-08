d8cc90425522ff329ac6c6ce92d0670e1abac0e1
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-09-08 17:52:27 +0530

    lorem added

--- react/src/pages/Writer.jsx
+++ react/src/pages/Writer.jsx
@@ -4,6 +4,7 @@ const Writer = () => {
   return (
     <div>
         <h1>This is the ai writer page</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi sed obcaecati at, omnis nulla dignissimos placeat nesciunt asperiores quidem minus harum suscipit eaque nostrum repellat, autem ullam, numquam ipsum! Doloremque?</p>
     </div>
   )
 }
