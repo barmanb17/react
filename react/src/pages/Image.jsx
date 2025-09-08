e39946fd8774f5146e80a4fd379a9dfbc4806258
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-09-08 17:50:12 +0530

    loren added

--- react/src/pages/Image.jsx
+++ react/src/pages/Image.jsx
@@ -4,6 +4,7 @@ const Image = () => {
   return (
     <div>
       <h1>This is the AI image page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique recusandae ipsa, id deleniti officiis earum eligendi, ipsum dignissimos laudantium itaque atque! Eos perspiciatis sunt rerum veritatis quae distinctio animi error!</p>
     </div>
   )
 }
