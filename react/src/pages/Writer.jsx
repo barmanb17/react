3160a5304a085a7fe6d12b7b2ccb6d3300efa1ed
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-09-09 23:45:43 +0530

    fix

--- react/src/pages/Writer.jsx
+++ react/src/pages/Writer.jsx
@@ -5,6 +5,7 @@ const Writer = () => {
     <div>
         <h1>This is the ai writer page</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi sed obcaecati at, omnis nulla dignissimos placeat nesciunt asperiores quidem minus harum suscipit eaque nostrum repellat, autem ullam, numquam ipsum! Doloremque?</p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia quo et, nemo, in facilis eaque sequi quidem temporibus ducimus cum culpa tenetur aliquam similique. Aspernatur incidunt totam soluta fuga voluptatem.
     </div>
   )
 }
