16452cb84ca6e51eae789ab06398d9a53a94644d
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-09-09 23:41:51 +0530

    fix

--- react/src/pages/NotFound.jsx
+++ react/src/pages/NotFound.jsx
@@ -4,6 +4,7 @@ const NotFound = () => {
   return (
     <div>NotFound
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint ut laborum atque error iste ipsum rem nam iusto, vitae necessitatibus accusamus voluptatem ab libero magni pariatur? Fugiat quod earum velit!</p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam repellat architecto iure nobis quae! Amet tempore qui quam illo enim at explicabo fuga odio corrupti libero ducimus, quis numquam placeat.
     </div>
     
   )
