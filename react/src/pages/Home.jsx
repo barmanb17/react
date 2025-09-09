f577699ada2eb6a7dc36ccb1357a780e37120da8
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-09-09 23:49:33 +0530

    links added

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -11,6 +11,9 @@ const Home = () => {
             <Link to="/resume">Resume</Link>
             <Link to="/shortlisting">Shortlisting</Link>
             <Link to="/notfound">NotFound</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/services">Services</Link>
         </nav>
     </div>
   )
