493497683c897b999fc56917ec1ae59d791df9d6
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-09-09 23:42:42 +0530

    lorem

--- react/src/pages/Resume.jsx
+++ react/src/pages/Resume.jsx
@@ -5,6 +5,7 @@ const Resume = () => {
     <div>
       <h1>This is the AI resume page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex beatae asperiores delectus rem suscipit, velit ullam saepe optio maxime esse earum? Beatae nesciunt quos illum ipsam velit. Optio, aliquam quia.</p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, exercitationem rerum nam neque repellat a? Facilis, doloribus velit architecto itaque dolorem quas quisquam, odit non amet voluptatum vel voluptatibus at?
     </div>
   )
 }
